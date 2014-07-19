a1241cad709d4d74917d6121362f1ff6e3dd4135
Lucas Mathis <lucasquinnmathis@gmail.com>
2014-07-19 12:17:40 -0700

    rectangle draws and moves around

--- js/asteroids.js
+++ js/asteroids.js
@@ -1,1 +1,28 @@
-var test = "sup"
+var canvas = document.getElementById('tutorial')
+var context = canvas.getContext('2d')
+context.fillStyle = "#000000"
+var x, y, width, height;
+
+x = 0;
+y = 0;
+width = 100;
+height = 100;
+
+function draw(){
+context.fillRect(x, y, width, height)
+}
+
+
+var moveRect = function(){
+  x += 1;
+  y += 1;
+}
+
+function drawInterval(){
+  setInterval(function(){
+    context.clearRect ( 0 , 0 , 600 , 600 );
+    draw()
+    moveRect()
+  }, 10)
+}
+
